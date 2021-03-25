579b80795e673216acbb91260ee72f3bb39b354c
elor1 <57156027+elor1@users.noreply.github.com>
2021-03-25 02:13:05 +0000

    Initial commit

---
+++ Source/MultiplayerGame/MultiplayerGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERGAME_API AMultiplayerGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
