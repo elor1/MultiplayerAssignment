cbff8d7481cacf3d08b836ad9e3d5d86f5151ad0
elor1 <57156027+elor1@users.noreply.github.com>
2021-04-16 03:37:19 +0100

    Added revolving door obstacle

---
+++ Source/MultiplayerGame/Actors/RevolvingDoor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "RevolvingDoor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERGAME_API ARevolvingDoor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+};
