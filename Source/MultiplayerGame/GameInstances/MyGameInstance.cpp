3c3b8bd7138dd7a83bde1b411c6cd13a4d3aadb8
elor1 <57156027+elor1@users.noreply.github.com>
2021-04-19 15:03:04 +0100

    Added host & join commands

---
+++ Source/MultiplayerGame/GameInstances/MyGameInstance.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+void UMyGameInstance::Host()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		World->ServerTravel("/Game/Maps/HoldingLevel?listen");
+	}
+}
+
+void UMyGameInstance::Join(const FString IPAddress)
+{
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (PlayerController)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Joining %s"), *IPAddress);
+		PlayerController->ClientTravel(IPAddress, ETravelType::TRAVEL_Absolute);
+	}
+}
+
+void UMyGameInstance::Warp(const FString LevelName)
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		FString Destination = FString::Printf(TEXT("Game/Maps/%s?listen"), *LevelName);
+		World->ServerTravel(Destination);
+	}
+}
---
+++ Source/MultiplayerGame/GameInstances/MyGameInstance.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+class UWidget;
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERGAME_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString IPAddress);
+
+	UFUNCTION(Exec)
+	void Warp(const FString LevelName);
+};
