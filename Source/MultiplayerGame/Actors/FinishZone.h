3bff292c46d087df728aaa1dc4a89a74bd49779c
elor1 <57156027+elor1@users.noreply.github.com>
2021-04-24 03:14:46 +0100

    Added firework particles when first person finishes

--- Source/MultiplayerGame/Actors/FinishZone.h
+++ Source/MultiplayerGame/Actors/FinishZone.h
@@ -42,6 +42,11 @@ private:
 	UPROPERTY(EditAnywhere)
 	UTextRenderComponent* Text;
 
+	bool bParticlesActivated = false;
+
 	UFUNCTION()
-		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void StartParticles();
 };
